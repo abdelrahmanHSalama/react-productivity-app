2cf99b51a34ec47f70a55177c3d816d4140ea6d6
Abdelrahman <AbdelrahmanHelmySalama@outlook.com>
2025-07-16 01:09:46 +0300

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,31 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { useTasks } from "./hooks/useTasks";
-import type { Task } from "./types/task";
+import { ConfigProvider } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import TasksPage from "./pages/TasksPage";
 
-function App() {
-  const [tasks, setTasks] = useState<Partial<Task>[]>([]);
-  const { fetchTasks } = useTasks();
-
-  useEffect(() => {
-    fetchTasks().then(setTasks);
-  }, []);
-
-  useEffect(() => {
-    console.log(tasks);
-  }, [tasks]);
-
+const App: React.FC = () => {
   return (
-    <main className="flex flex-col justify-center items-center h-screen gap-2">
-      <h1 className="text-2xl hover:*:block flex items-center">
-        <span className="border p-0.5 mr-0.5 rounded">M</span>
-        <span className="hidden">otion</span>
-      </h1>
-      {tasks.map((t) => (
-        <div className="bg-gray-100 p-2 rounded flex gap-1" key={t.id}>
-          <p> {t.title}</p>
-          <button className="border rounded-full aspect-square hover:bg-black hover:text-gray-50 cursor-pointer">
-            ×
-          </button>
-        </div>
-      ))}
-    </main>
+    <ConfigProvider
+      theme={{
+        token: {},
+        components: {
+          Menu: {
+            algorithm: true, // Enable algorithm
+          },
+        },
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {/* <Route index element={<Home />} /> */}
+            <Route path="tasks" element={<TasksPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
---
+++ src/utils/tasksService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import type { Task } from "../types/task";
+
+export const fetchTasks = async () => {
+  const res = await axios.get("http://localhost:3000/tasks");
+  return res.data;
+};
+
+export const addTask = async (task: Task) => {
+  const res = await axios.post("http://localhost:3000/tasks", task);
+  return res.data;
+};
+
+export const updateTask = async (id: string, task: Task) => {
+  const res = await axios.put(`http://localhost:3000/tasks/${id}`, task);
+  return res.data;
+};
+
+export const deleteTask = async (id: string) => {
+  await axios.delete(`http://localhost:3000/tasks/${id}`);
+};
